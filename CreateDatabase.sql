b752e33822269f555ae3b6a5100b733cdfe1756a
martinmolema <martin@molema.org>
2021-11-21 19:36:51 +0100

    Added script for basic database creation

---
+++ CreateDatabase.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS tbl_configuration;
+
+CREATE TABLE tbl_configuration
+(
+    url     VARCHAR(200) NOT NULL COMMENT 'Home page URL',
+    version VARCHAR(10)  NOT NULL COMMENT 'Version number'
+) COMMENT  'Configuration';
+
+INSERT INTO tbl_configuration (url, version)
+VALUES ('http://energy.org',
+        '0.1');
