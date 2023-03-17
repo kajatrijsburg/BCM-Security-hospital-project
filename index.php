cf7995520720cc9634fc13c6e5d469d7d9a89b03
Stefan366 <91754308+Stefan366@users.noreply.github.com>
2023-03-17 14:35:15 +0100

    the made layout and pages in visual studio code is transferred to the project

--- index.php
+++ index.php
@@ -1,41 +1,150 @@
-<?php
-/** @file index.php
- * Index for the public website
- *
- * @author Martin Molema <martin.molema@nhlstenden.com>
- * @copyright 2022
- *
- * Show a very basis HTML Bootstrap template
- */
-?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Energy Portal</title>
-    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/index.css" rel="stylesheet">
-    <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light" role="navigation">
-    <a class="navbar-brand" href="#">
-        <img src="/images/logo.png" width="30" height="30" alt="">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    <link href="css/main.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"
+    />
+   
+    
+    <title>Frontend Bootcamp</title>
+  </head>
+  <body>
+    <header>
+      <!-- Navbar -->
+      <nav
+      id="main-navbar"
+      class="navbar navbar-expand-lg navbar-light bg-danger top"
+      >
+   <!-- Container wrapper -->
+   <div class="container-fluid d-flex justify-content-center">
+     
+
+     <!-- Brand -->
+     <a class="navbar-brand" href="#">
+       <img
+            src="Image/logo.svg"
+            height="50"
+            alt=""
+            loading="lazy"
+            />
+     </a>
+     <a class="navbar-brand" href="#">
+      <img
+           src="Image/Naamloos.png"
+           height="50"
+           alt=""
+           loading="lazy"
+           />
     </a>
-    <ul class="navbar-nav mr-auto">
-        <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
-        <li class="nav-item"><a href="/intranet" class="nav-link">Intranet</a></li>
-    </ul>
-</nav>
-<main class="container-fluid">
-    <article class="main">
-        <header><h1>Welkom bij onze website</h1></header>
-        <section>
-            Dit is onze website.
-        </section>
-    </article>
-</main>
-<? include_once './partials/footer.php'; ?>
-</body>
+    
+
+       
+       
+      
+   <!-- Container wrapper -->
+ </nav>
+ <!-- Navbar -->
+        <!-- Sidebar -->
+        <div class="container-fluid ">
+            <div class="row flex-nowrap ">
+                <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+                    <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                        <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                            <span class="fs-5 d-none d-sm-inline">Menu</span>
+                        </a>
+                        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                            <li class="nav-item">
+                                <a href="/" class="nav-link align-middle px-0">
+                                    <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Dashboard</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="Behandelplan.php" class="nav-link px-0 align-middle">
+                                    <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Behandelplan</span> </a>
+                                
+                            </li>
+                            <li class="nav-item">
+                                <a href="Afspraken.php" class="nav-link px-0 align-middle">
+                                    <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Afspraken</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="Medicijnen.php" class="nav-link px-0 align-middle ">
+                                    <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 d-none d-sm-inline">Medicijnen</span></a>
+                                <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
+                                    <li class="w-100 nav-item">
+                                        <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <hr>
+                        <div class="dropdown pb-5">
+                            <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle pb-5" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
+                                <span class="d-none d-sm-inline mx-1">loser</span>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+                                <li><a class="dropdown-item" href="#">New project...</a></li>
+                                <li><a class="dropdown-item" href="#">Settings</a></li>
+                                <li><a class="dropdown-item" href="#">Profile</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Sign out</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="card border-dark w-100 h-100">
+                    <table>
+                        <tr>
+                            <th>beschrijving</th>
+                            <th>✖</th>
+                            <th>✓</th>
+                        </tr>
+                        <?php
+                        //foreach (ModelItem in databaseModel)
+                        {
+                            //<tr>
+                            //<td></td>  description of the procedure
+                            //<td></td>  radio type input for decline
+                            //<td></td>  radio type input for approval
+                            //</tr>
+                        }
+                        ?>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+        <!-- Sidebar -->
+     
+      </header>
+      <!--Main Navigation-->
+      
+      <!--Main layout-->
+      <main style="margin-top: 58px">
+        <div class="container pt-4">
+      
+        </div>
+      </main>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+      crossorigin="anonymous"
+    ></script>
+    
+
+  </body>
 </html>
