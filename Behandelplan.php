da68f22079461db1dec82b506b74ca1c8d9f7a58
kajatrijsburg <kajatrijsburg@gmail.com>
2023-03-19 11:46:25 +0100

    bootstrap now correctly linked on all pages

--- Behandelplan.php
+++ Behandelplan.php
@@ -7,6 +7,7 @@
     
     <link href="css/main.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
+      <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"
