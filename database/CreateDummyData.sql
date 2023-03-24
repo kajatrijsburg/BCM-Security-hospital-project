d920d80c4bf5fe9c2d97ef4f7b5173b3b8554644
kajatrijsburg <kajatrijsburg@gmail.com>
2023-03-24 15:02:09 +0100

    adds dummy data script

---
+++ database/CreateDummyData.sql
@@ -0,0 +1,6 @@
+INSERT INTO user (voornaam, achternaam, email) VALUES ('FirstName', 'lastName', 'exampleemail@test.com');
+INSERT INTO user (voornaam, achternaam, email) VALUES ('DifferentFirstName', 'DifferentFirstName', 'differentexampleemail@test.com');
+
+INSERT INTO patienten (specialistid, patientid) VALUES ('1', '2');
+
+INSERT INTO afspraken (locatie, online, datum, tijd, specialistid, patientid) VALUES ('TestLocatie', 0, '2023-04-05', '13 15', 2, 1);
